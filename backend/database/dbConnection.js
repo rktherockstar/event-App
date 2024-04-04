b303efecd971b2a54e39a9e3bec7b2ffee34b2e1
Ramkrishna goud <ramakrishnagoud9199@gmail.com>
2024-04-04 21:03:09 +0530

    lcommit

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const dbConnection = () => {
   mongoose
-    .connect(process.env.MONGO_URI, { dbName: "MERN_EVENT" })
+    .connect(process.env.MONGO_URI, { dbName: "MERN_STACK" })
     .then(() => {
       console.log("Connected to database!");
     })
